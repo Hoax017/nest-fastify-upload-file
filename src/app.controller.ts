6cf73a02e6657e7b979f690b156534ec9d82dd07
Aymeric <aymeric@bob-desk.fr>
2022-05-10 14:45:39 +0200

    :sparkles: upload multiple files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, UseInterceptors, Request } from '@nestjs/common';
+import {
+  FilesInterceptor,
+  StorageFile,
+  UploadedFiles,
+} from '@blazity/nest-file-fastify';
+import * as fs from 'fs';
+import * as stream from 'stream';
+import { join } from 'path';
+import { extension } from 'mime-types';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload')
+  @UseInterceptors(
+    FilesInterceptor('files', 10, {
+      dest: join(__dirname, '..', '/src/public/upload'),
+    }),
+  )
+  async uploadFile(@UploadedFiles() files: Array<{ filename: string }>) {
+    return files.map((file) => {
+      return { url: '/upload/' + file.filename };
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import multipart from 'fastify-multipart';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.register(multipart);
+
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then(() => console.log('Server running on port 3000'));
